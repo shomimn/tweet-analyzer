697e3285a25786e0a005a42971e656e549e21097
shomimn <shomimn@gmail.com>
2016-04-01 23:03:22 +0200

    temp

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,3 +1,16 @@
+import com.esri.core.geometry.Envelope;
+import com.esri.core.geometry.Envelope2D;
+import com.esri.core.geometry.Geometry;
+import com.esri.core.geometry.OperatorBuffer;
+import com.esri.core.geometry.Point;
+import com.esri.core.geometry.Point2D;
+import com.esri.core.geometry.QuadTree;
+import com.esri.core.geometry.SpatialReference;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.topology.TopologyBuilder;
@@ -8,6 +21,7 @@ import bolt.ResultBolt;
 import bolt.TimePointBolt;
 import bolt.TimeUnitBolt;
 import spout.TwitterSpout;
+import util.POI;
 import util.TimeFragmenter;
 
 public class Main
@@ -25,36 +39,85 @@ public class Main
         String accessTokenSecret = "iRgGshI44MsSmc8tqzmApqzBz0fPBCm8HL1rUVK1s7nn4";
         String[] keyWords = new String[] { };
 
-        TimeFragmenter fragmenter = new TimeFragmenter(DURATION, TIME_UNITS);
+        BufferedReader reader = new BufferedReader(new FileReader("/home/milos/storm/tweet-analyzer/new-york.csv"));
+        String line;
+        ArrayList<POI> pois = new ArrayList<>();
+        double xmax = Double.MIN_VALUE;
+        double ymax = Double.MIN_VALUE;
+        double xmin = Double.MAX_VALUE;
+        double ymin = Double.MAX_VALUE;
+        while ((line = reader.readLine()) != null)
+        {
+            String[] tokens = line.split("\\|");
+            double lat = Double.parseDouble(tokens[2]);
+            double lng = Double.parseDouble(tokens[3]);
 
-        builder.setSpout(TwitterSpout.ID, new TwitterSpout(consumerKey, consumerSecret,
-                accessToken, accessTokenSecret, keyWords));
+            if (lat > xmax)
+                xmax = lat;
+            if (lat < xmin)
+                xmin = lat;
+            if (lng > ymax)
+                ymax = lng;
+            if (lng < ymin)
+                ymin = lng;
 
-        builder.setBolt(PrinterBolt.ID, new PrinterBolt())
-                .shuffleGrouping(TwitterSpout.ID);
+            pois.add(new POI(lat, lng, tokens[4]));
+            System.out.println(line);
+        }
 
-        builder.setBolt(TimeUnitBolt.ID, new TimeUnitBolt(fragmenter))
-                .shuffleGrouping(PrinterBolt.ID, PrinterBolt.TIME_UNIT_STREAM);
+        QuadTree quadTree = new QuadTree(new Envelope2D(xmin, ymin, xmax, ymax), 16);
+        SpatialReference ref = SpatialReference.create(4326);
+        for (int i = 0; i < pois.size(); ++i)
+        {
+            Envelope envelope = new Envelope();
+            Point p = new Point(pois.get(i).getLatitude(), pois.get(i).getLongitude());
+            Geometry geom = OperatorBuffer.local().execute(p, ref, 0.00011, null);
+            geom.queryEnvelope(envelope);
 
-        builder.setBolt(PlaceBolt.ID, new PlaceBolt())
-                .shuffleGrouping(PrinterBolt.ID, PrinterBolt.PLACE_STREAM);
+            quadTree.insert(i, new Envelope2D(envelope.getXMin(), envelope.getYMin(),
+                    envelope.getXMax(), envelope.getYMax()));
+        }
 
-        builder.setBolt(TimePointBolt.ID, new TimePointBolt(fragmenter))
-                .shuffleGrouping(PrinterBolt.ID, PrinterBolt.TIME_POINT_STREAM);
+        QuadTree.QuadTreeIterator it = quadTree.getIterator(new Point(40.73824, -74.00429), 0);
+        int handle = it.next();
+        while (handle >= 0)
+        {
+            int element = quadTree.getElement(handle);
+            System.out.println(pois.get(element).getName());
+            handle = it.next();
+        }
 
-        builder.setBolt(ResultBolt.ID, new ResultBolt(DURATION, TIME_UNITS), 1)
-                .shuffleGrouping(TimeUnitBolt.ID, TimeUnitBolt.STREAM)
-                .shuffleGrouping(PlaceBolt.ID, PlaceBolt.STREAM)
-                .shuffleGrouping(TimePointBolt.ID, TimePointBolt.STREAM);
 
-        Config config = new Config();
-//        config.setDebug(true);
-
-        LocalCluster cluster = new LocalCluster();
-
-        cluster.submitTopology("test", config, builder.createTopology());
-
-        Utils.sleep(DURATION);
+//        TimeFragmenter fragmenter = new TimeFragmenter(DURATION, TIME_UNITS);
+//
+//        builder.setSpout(TwitterSpout.ID, new TwitterSpout(consumerKey, consumerSecret,
+//                accessToken, accessTokenSecret, keyWords));
+//
+//        builder.setBolt(PrinterBolt.ID, new PrinterBolt())
+//                .shuffleGrouping(TwitterSpout.ID);
+//
+//        builder.setBolt(TimeUnitBolt.ID, new TimeUnitBolt(fragmenter))
+//                .shuffleGrouping(PrinterBolt.ID, PrinterBolt.TIME_UNIT_STREAM);
+//
+//        builder.setBolt(PlaceBolt.ID, new PlaceBolt())
+//                .shuffleGrouping(PrinterBolt.ID, PrinterBolt.PLACE_STREAM);
+//
+//        builder.setBolt(TimePointBolt.ID, new TimePointBolt(fragmenter))
+//                .shuffleGrouping(PrinterBolt.ID, PrinterBolt.TIME_POINT_STREAM);
+//
+//        builder.setBolt(ResultBolt.ID, new ResultBolt(DURATION, TIME_UNITS), 1)
+//                .shuffleGrouping(TimeUnitBolt.ID, TimeUnitBolt.STREAM)
+//                .shuffleGrouping(PlaceBolt.ID, PlaceBolt.STREAM)
+//                .shuffleGrouping(TimePointBolt.ID, TimePointBolt.STREAM);
+//
+//        Config config = new Config();
+////        config.setDebug(true);
+//
+//        LocalCluster cluster = new LocalCluster();
+//
+//        cluster.submitTopology("test", config, builder.createTopology());
+//
+//        Utils.sleep(DURATION);
 
 //        cluster.shutdown();
     }
---
+++ untitled/src/util/POI.java
@@ -0,0 +1,45 @@
+package util;
+
+public class POI
+{
+    private double latitude;
+    private double longitude;
+    private String name;
+
+    public POI(double lat, double lng, String n)
+    {
+        latitude = lat;
+        longitude = lng;
+        name = n;
+    }
+
+    public double getLatitude()
+    {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude()
+    {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude)
+    {
+        this.longitude = longitude;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
